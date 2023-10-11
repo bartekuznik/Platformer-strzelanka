350567c6e53fd15588e1a56f34d2d6302512bdca
bartekuznik <bartek.krecio@onet.pl>
2023-10-11 20:07:13 +0200

    add player movement and collisions

--- level.py
+++ level.py
@@ -4,13 +4,13 @@ from player import *
 
 level_date = [["w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
          ["w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
-         ["w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
+         ["w", "d", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
          ["w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "g", "g", "g", "g", "w", "w"],
          ["w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
          ["w", "w", "w", "w", "w", "w", "w", "w", "w", "g", "g", "g", "g", "w", "w", "w", "w", "w", "w", "w"],
          ["w", "w", "w", "w", "w", "w", "w", "g", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
          ["w", "w", "w", "w", "w", "w", "g", "g", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
-         ["w", "d", "w", "w", "w", "w", "g", "g", "g", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
+         ["w", "w", "w", "w", "w", "w", "g", "g", "g", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w", "w"],
          ["g", "g", "g", "g", "g", "g", "g", "g", "g", "g", "w", "w", "g", "g", "g", "g", "g", "g", "g", "g"],
          ]
 block_size = 60
@@ -35,9 +35,40 @@ class LevelBase():
                     self.player = Player((x,y))
                     self.player_group.add(self.player)
                     
-        
+    def block_collide(self, blocks):
+        block_collision = []
+        for block in blocks:
+            if self.player.rect.colliderect(block):
+                block_collision.append(block)
+        return block_collision
+    
+    def horizontal_collisions(self):
+        self.player.rect.x += self.player.player_move[0]
+        coll_block = self.block_collide(self.blocks)
+        for block in coll_block:
+            if self.player.player_move[0] > 0 and self.player.moving_right == True:
+                self.player.rect.right = block.rect.left
+                print("PRAWO")
+            if self.player.player_move[0] < 0 and self.player.moving_left == True:
+                self.player.rect.left = block.rect.right
+                print("LEWO")
+    
+    def vertical_collisions(self):
+        self.player.add_gravity()
+        self.player.rect.y += self.player.player_move[1]
+        coll_block = self.block_collide(self.blocks)
+        for block in coll_block:
+            if self.player.player_move[1] > 0:
+                self.player.rect.bottom = block.rect.top
+                #self.player.player_move[1] = 0
+            if self.player.player_move[1] < 0:
+                self.player.rect.top = block.rect.bottom
+                
     def update(self):
         self.blocks.draw(self.screen)
         self.player_group.draw(self.screen)
+        self.block_collide(self.blocks)
+        self.horizontal_collisions()
+        self.vertical_collisions()
         self.player.update()
 
--- player.py
+++ player.py
@@ -3,28 +3,67 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self, position):
         super().__init__()
-        self.image = pygame.Surface((20,40))
+        self.image = pygame.Surface((20,60))
         self.rect = self.image.get_rect(topleft = position)
         self.image.fill('blue')
         self.gravity = 0
-        self.jump_value = -40
+        self.jump_value = -15
+        self.moving_right = False
+        self.moving_left = False
+        self.y_momentum = 0
+        self.player_move = [position[0], position[1]]
 
     def add_gravity(self):
-        self.gravity += 3
-        self.rect.y += self.gravity
+        """ if self.y_momentum < 1:
+            self.y_momentum += 0.2
+        else:
+            self.y_momentum = 1
+        self.player_move[1] = self.y_momentum"""
+    
+        self.player_move[1] += self.y_momentum
+        self.y_momentum += 0.5
+        if self.y_momentum > 10:
+            self.y_momentum = 10
 
     def jump(self):
-        self.gravity = self.jump_value
+        self.y_momentum = self.jump_value
 
-    def moving(self):
+    def player_input(self):
         keys = pygame.key.get_pressed()
         if keys[pygame.K_LEFT]:
-            self.rect.x -= 7
-        if keys[pygame.K_RIGHT]:
-            self.rect.x += 7
+            self.moving_left = True
+            self.moving_right = False
+        elif keys[pygame.K_RIGHT]:
+            self.moving_right = True
+            self.moving_left = False
+        else:
+            self.moving_right = False
+            self.moving_left = False
+
         if keys[pygame.K_SPACE]:
             self.jump()
+            print(self.player_move[1])
+
+
+    def moving(self):
+        self.player_move = [0, 0]
+        if self.moving_right == True:
+            self.player_move[0] += 7
+        if self.moving_left == True:
+            self.player_move[0] -= 7
+
+        print(self.player_move)
+
+    """def test_fuction(self):
+        if self.player_move[1] > 600 - self.image.get_height(): # 600 - wyskość okna
+            self.y_momentum = - self.y_momentum
+        else:
+            self.y_momentum += 0.2
+        self.player_move[1] += self.y_momentum"""
 
     def update(self):
-        self.add_gravity()
+        self.player_input()
         self.moving()
+        #self.add_gravity()
+        #self.test_fuction()
+        #print(self.rect.y,self.rect.x)
